c66164ecf982e5e8cf506287abd5dcf9e8889601
ctyq77 <1340766270@qq.com>
2026-06-25 03:41:16 +0800

    Initial commit

---
+++ src/main/java/com/ai/aicodeapplicationgeneration/AIcodeApplicationGenerationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ai.aicodeapplicationgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIcodeApplicationGenerationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIcodeApplicationGenerationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/aicodeapplicationgeneration/AIcodeApplicationGenerationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.aicodeapplicationgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIcodeApplicationGenerationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
